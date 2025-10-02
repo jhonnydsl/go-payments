add8c3bea840855eef650c105b9f138338fa3ea3
jhonnydsl <jhonny.s.l.12345@gmail.com>
2025-10-02 02:36:24 -0300

    Refactor LoginUser to use utils for password verification and JWT generation

--- src/services/user.go
+++ src/services/user.go
@@ -2,15 +2,12 @@ package services
 
 import (
 	"context"
+	"errors"
 	"fmt"
-	"os"
-	"time"
 
-	"github.com/golang-jwt/jwt/v5"
 	"github.com/jhonnydsl/payment-API/src/dtos"
 	"github.com/jhonnydsl/payment-API/src/repository"
 	"github.com/jhonnydsl/payment-API/src/utils"
-	"golang.org/x/crypto/bcrypt"
 )
 
 type UserService struct {
@@ -28,24 +25,16 @@ func (service *UserService) CreateUser (ctx context.Context, user dtos.UserInput
 func (service *UserService) LoginUser(ctx context.Context, login dtos.UserLogin) (string, error) {
 	userLogin, password, err := service.Repo.GetUserByEmail(ctx, login.Email)
 	if err != nil {
-		return "", fmt.Errorf("email or password invalid")
+		return "", errors.New("email or password invalid")
 	}
 
-	err = bcrypt.CompareHashAndPassword([]byte(password), []byte(login.Password))
+	if err := utils.CheckPassword(password, login.Password); err != nil {
+		return "", errors.New("email or password invalid")
+	}
+
+	tokenStr, err := utils.GenerateJWT(userLogin.ID, userLogin.Email)
 	if err != nil {
-		return "", fmt.Errorf("email or password invalid")
-	}
-
-	claims := jwt.MapClaims{
-		"user_id": userLogin.ID,
-		"email": userLogin.Email,
-		"exp": time.Now().Add(time.Hour *24).Unix(),
-	}
-
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	tokenStr, err := token.SignedString([]byte(os.Getenv("JWT_SECRET")))
-	if err != nil {
-		return "", fmt.Errorf("error generating token")
+		return "", fmt.Errorf("error generating token: %w", err)
 	}
 
 	return tokenStr, nil
