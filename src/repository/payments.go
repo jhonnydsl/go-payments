e2d6e0ecc820ab7cb9260719b31b2d6217d882a4
jhonnydsl <jhonny.s.l.12345@gmail.com>
2025-10-08 00:47:51 -0300

    refactor: update CreatePayment function in PaymentRepository

--- src/repository/payments.go
+++ src/repository/payments.go
@@ -10,14 +10,14 @@ type PaymentsRepository struct{}
 
 func (r *PaymentsRepository) CreatePayment(ctx context.Context, payment dtos.PaymentInput, userID int) (dtos.PaymentOutput, error) {
 	query := `
-	INSERT INTO payments (user_id, amount, currency, payment_method)
-	VALUES ($1, $2, $3, $4)
-	RETURNING id, user_id, amount, currency, payment_method, status, created_at, updated_at;
+	INSERT INTO payments (user_id, amount, currency, payment_method_id, status)
+	VALUES ($1, $2, $3, $4, $5)
+	RETURNING id, user_id, amount, currency, payment_method_id, status, created_at, updated_at;
 	`
 	var createdPayment dtos.PaymentOutput
 
 
-	err := DB.QueryRowContext(ctx, query, userID, payment.Amount, payment.Currency, payment.PaymentMethodID).Scan(
+	err := DB.QueryRowContext(ctx, query, userID, payment.Amount, payment.Currency, payment.PaymentMethodID, "pending").Scan(
 		&createdPayment.ID,
 		&createdPayment.UserID,
 		&createdPayment.Amount,
